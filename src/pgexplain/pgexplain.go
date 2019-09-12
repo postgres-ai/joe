033ae85bd5f8756dc0655fd92c274c83eaefe680
Nikolay Samokhvalov <gitlab@samokhvalov.com>
2019-09-12 10:22:40 +0000

    "min" instead of "m"

--- src/pgexplain/pgexplain.go
+++ src/pgexplain/pgexplain.go
@@ -519,7 +519,7 @@ func durationToString(value float64) string {
 	} else if value < 60000 {
 		return fmt.Sprintf("%.3f s", value/1000.0)
 	} else {
-		return fmt.Sprintf("%.3f m", value/60000.0)
+		return fmt.Sprintf("%.3f min", value/60000.0)
 	}
 }
 
