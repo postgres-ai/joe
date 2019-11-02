postgres-ai/joe

commit 26d51c369812cb75b2c4117aeae34bc896e541e7

fix: configs and bugs

src/provision/provision.go
@@ -126,9 +126,27 @@ func IsValidConfig(c Config) bool {
 }
 
 func (s *Session) GetConnStr(dbname string) string {
-	connStr := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		s.Host, s.Port, s.User, s.Password, dbname)
+	connStr := "sslmode=disable"
+
+	if len(s.Host) > 0 {
+		connStr += " " + s.Host
+	}
+
+	if s.Port > 0 {
+		connStr += fmt.Sprintf(" %d", s.Port)
+	}
+
+	if len(s.User) > 0 {
+		connStr += " " + s.User
+	}
+
+	if len(s.Password) > 0 {
+		connStr += " " + s.Password
+	}
+
+	if len(dbname) > 0 {
+		connStr += " " + dbname
+	}
 
 	return connStr
 }

config/explain.yaml
@@ -33,5 +33,5 @@ tips:
     detailsUrl: "https://postgres.ai/#tip-index-inefficient-high-filtered"
   - code: "VACUUM_ANALYZE_NEEDED"
     name: "VACUUM ANALYZE needed"
-    description: "Visibility map(s) for the table(s) involved in the query are outdated. For better performance: 1) run `VACUUM ANALYZE` on them as a one-time solution, 2) perform autovacuum tuning as a long-term permanent solution (tuning "vacuum" part of autovacuum)."
+    description: "Visibility map(s) for the table(s) involved in the query are outdated. For better performance: 1) run `VACUUM ANALYZE` on them as a one-time solution, 2) perform autovacuum tuning as a long-term permanent solution (tuning \"vacuum\" part of autovacuum)."
     detailsUrl: "https://postgres.ai/#tip-vacuum-analyze-needed"

config/provisioning.mulocal.sample.yaml
@@ -7,7 +7,7 @@ mulocal:
     to: 6100
 debug: true
 pgVersion: "11"
-pgBindir: "/usr/lib/postgresql/9.6/bin/pg_ctl"
+pgBindir: "/usr/lib/postgresql/9.6/bin"
 pgDataSubdir: "/pgdata"
 zfsPool: "joepool"
 initialSnapshot: "db_state_1"

src/bot/bot.go
@@ -135,10 +135,6 @@ type Config struct {
 	QuotaInterval uint // Seconds.
 	IdleInterval  uint // Seconds.
 
-	DbHost     string
-	DbPort     uint
-	DbUser     string
-	DbPassword string
 	DbName     string
 
 	ApiUrl         string

src/main.go
@@ -223,12 +223,12 @@ func loadProvisionConfig() (ProvisionConfig, error) {
 func loadConfig(config interface{}, name string) error {
 	b, err := ioutil.ReadFile(getConfigPath(name))
 	if err != nil {
-		return fmt.Errorf("Error loading %s config file", name)
+		return fmt.Errorf("Error loading %s config file: %v", name, err)
 	}
 
 	err = yaml.Unmarshal(b, config)
 	if err != nil {
-		return fmt.Errorf("Error parsing %s config", name)
+		return fmt.Errorf("Error parsing %s config: %v", name, err)
 	}
 
 	log.Dbg("Config loaded", name, config)

commit 7ec01d07fe0010f0b3e3bb7c50f669c8362522a8

Fix connStr
https://gitlab.com/postgres-ai/joe/commit/26d51c369812cb75b2c4117aeae34bc896e541e7 introduced a bug leading to errors like:
```
ERROR: missing "=" after "localhost" in connection info string"
```

This is a fix for it.

src/provision/provision.go
@@ -129,23 +129,23 @@ func (s *Session) GetConnStr(dbname string) string {
 	connStr := "sslmode=disable"
 
 	if len(s.Host) > 0 {
-		connStr += " " + s.Host
+		connStr += " host=" + s.Host
 	}
 
 	if s.Port > 0 {
-		connStr += fmt.Sprintf(" %d", s.Port)
+		connStr += fmt.Sprintf(" port=%d", s.Port)
 	}
 
 	if len(s.User) > 0 {
-		connStr += " " + s.User
+		connStr += " user=" + s.User
 	}
 
 	if len(s.Password) > 0 {
-		connStr += " " + s.Password
+		connStr += " password=" + s.Password
 	}
 
 	if len(dbname) > 0 {
-		connStr += " " + dbname
+		connStr += " dbname=" + dbname
 	}
 
 	return connStr