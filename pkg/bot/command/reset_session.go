b6e5d4d0bf43929ea1a1db1332ef5399e4c64c54
Artyom Kartasov <agneum@gmail.com>
2020-08-07 12:45:36 +0000

    fix: close client-side connections after session reset (#160)

--- pkg/bot/command/reset_session.go
+++ pkg/bot/command/reset_session.go
@@ -7,6 +7,8 @@ package command
 import (
 	"context"
 
+	"github.com/jackc/pgx/v4/pgxpool"
+
 	"gitlab.com/postgres-ai/database-lab/pkg/client/dblabapi"
 	"gitlab.com/postgres-ai/database-lab/pkg/log"
 
@@ -17,8 +19,7 @@ import (
 
 // ResetSession provides a command to reset a Database Lab session.
 func ResetSession(ctx context.Context, cmd *platform.Command, msg *models.Message, dbLab *dblabapi.Client, cloneID string,
-	msgSvc connection.Messenger) error {
-
+	msgSvc connection.Messenger, db *pgxpool.Pool) error {
 	msg.AppendText("Resetting the state of the database...")
 	msgSvc.UpdateText(msg)
 
@@ -27,6 +28,15 @@ func ResetSession(ctx context.Context, cmd *platform.Command, msg *models.Messag
 	if err := dbLab.ResetClone(ctx, cloneID); err != nil {
 		log.Err("Reset:", err)
 		return err
+	}
+
+	allIdleConnections := db.AcquireAllIdle(ctx)
+	for _, idleConnection := range allIdleConnections {
+		if err := idleConnection.Conn().Close(ctx); err != nil {
+			log.Err("Failed to close idle connection: ", err)
+		}
+
+		idleConnection.Release()
 	}
 
 	result := "The state of the database has been reset."
